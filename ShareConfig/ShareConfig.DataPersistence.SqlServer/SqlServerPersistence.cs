4052f81aab2e87c819e7d2ff68d829f8cce7442f
axzxs2001 <axzxs2001@163.com>
2018-04-26 10:22:02 +0800

    重构数据持久化类。

--- ShareConfig/ShareConfig.DataPersistence.SqlServer/SqlServerPersistence.cs
+++ ShareConfig/ShareConfig.DataPersistence.SqlServer/SqlServerPersistence.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using ShareConfig.Core;
+using System.Data.SqlClient;
 
 namespace ShareConfig.DataPersistence.SqlServer
 {
@@ -11,12 +12,54 @@ namespace ShareConfig.DataPersistence.SqlServer
         {
             _connectionString = connectionString;
         }
-        public Dictionary<Key, T> ReadConfigs<T>() where T : class, new()
+        public Dictionary<Key, dynamic> ReadConfigs() 
         {
-            throw new NotImplementedException();
+            using(var conn=new SqlConnection(_connectionString))
+            {
+                var cmd = new SqlCommand();
+                cmd.Connection = conn;
+                cmd.CommandText = "";
+
+                var reader=cmd.ExecuteReader();
+                var configDic = new Dictionary<Key, dynamic>();
+                while(reader.Read())
+                {
+                    var keyString= reader.GetString(0);
+                    var valueJson = reader.GetString(1);
+                    var key = ToKey(keyString);
+                    var value = Newtonsoft.Json.JsonConvert.DeserializeObject<dynamic>(valueJson);
+                    configDic.Add(key, value);
+
+                }
+                return configDic;
+            }
+        }
+        Key ToKey(string keyString)
+        {
+           var keyArr= keyString.Split('/');
+            var key = new Key();
+            key.NameSpace = keyArr[0];
+            key.Environment = keyArr[1];
+            key.Version = keyArr[2];
+            key.Tag = keyArr[3];
+            return key;
+        }
+        /// <summary>
+        /// if table is not exist ,creates the table.
+        /// </summary>
+        void CreateTable()
+        {
+            using (var conn = new SqlConnection(_connectionString))
+            {
+                var cmd = new SqlCommand();
+                cmd.Connection = conn;
+                cmd.CommandText = @"";
+
+                cmd.ExecuteNonQuery();
+            }
         }
 
-        public bool WriteConfigs<T>(Dictionary<Key, T> configs) where T : class, new()
+        public bool WriteConfigs(Dictionary<Key, dynamic> configs)
         {
             throw new NotImplementedException();
         }
